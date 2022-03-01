1bcf3b08db0aa413ff7fdd200057f4912af1ef97
badis-naceur <naceurbadis@gmail.com>
2022-03-01 19:02:19 +0100

    badis

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './login.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./login.css";
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
@@ -30,10 +30,11 @@ const Login = () => {
           response.data === "Invalid Email or Password"
         ) {
           alert("Invalid Password");
-          navigate.push("/login");
-        } else if (response.data === "logged in successfully")
+          // navigate.push("/login");
+        } else if (response.msg === "logged in successfully")
           setUser(data.email);
-        navigate("/profil");
+          navigate("/PostBlogs");
+          localStorage.setItem("doctor",JSON.stringify(response.data.token));
         // console.log('user',user)
       });
     } catch (error) {
@@ -85,43 +86,73 @@ const Login = () => {
       <div className="maincontainer">
         <div class="container-fluid">
           <div class="row no-gutter">
-
             <div class="col-md-6 d-none d-md-flex bg-image"></div>
 
             <div class="col-md-6 bg-light">
               <div class="login d-flex align-items-center py-5">
-
                 <div class="container">
                   <div class="row">
-                    <div class="col-lg-10 col-xl-7 mx-auto" >
+                    <div class="col-lg-10 col-xl-7 mx-auto">
                       <h3 class="display-4">Welcome!</h3>
-                      <p class="text-muted mb-4">Fill the form with your email and password</p>
+                      <p class="text-muted mb-4">
+                        Fill the form with your email and password
+                      </p>
                       <form onSubmit={handleSubmit}>
                         <div class="mb-3">
-                          <input id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" type="email"
+                          <input
+                            id="inputEmail"
+                            type="email"
+                            placeholder="Email address"
+                            required=""
+                            autofocus=""
+                            class="form-control rounded-pill border-0 shadow-sm px-4"
+                            type="email"
                             placeholder="Email"
                             name="email"
                             onChange={handleChange}
                             value={data.email}
-                            required />
+                            required
+                          />
                         </div>
                         <div class="mb-3">
-                          <input id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" type="password"
+                          <input
+                            id="inputPassword"
+                            type="password"
+                            placeholder="Password"
+                            required=""
+                            class="form-control rounded-pill border-0 shadow-sm px-4 text-primary"
+                            type="password"
                             placeholder="Password"
                             name="password"
                             onChange={handleChange}
                             value={data.password}
-                            required />
+                            required
+                          />
                         </div>
                         <div class="form-check">
-                          <input id="customCheck1" type="checkbox" checked class="form-check-input" />
-                          <label for="customCheck1" class="form-check-label">Remember password</label>
+                          <input
+                            id="customCheck1"
+                            type="checkbox"
+                            checked
+                            class="form-check-input"
+                          />
+                          <label for="customCheck1" class="form-check-label">
+                            Remember password
+                          </label>
                         </div>
                         <div class="d-grid gap-2 mt-2">
-                          <button type="submit" class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
+                          <button
+                            onClick={handleSubmit}
+                            type="submit"
+                            class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm"
+                          >
+                            Sign in
+                          </button>
                         </div>
                         <Link to="/signup">
-                        <div class="text-center d-flex justify-content-between mt-4"><p>You don't have an account yet?</p></div>
+                          <div class="text-center d-flex justify-content-between mt-4">
+                            <p>You don't have an account yet?</p>
+                          </div>
                         </Link>
                       </form>
                     </div>
---
+++ src/Components/logout.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+
+const Logout =()=>{
+  
+  
+};
